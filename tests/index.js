fc906b71f36e80157026594d66ff4d4173b4813f
rynop <ryan.pendergast@gmail.com>
2017-08-03 22:35:01 -0500

    initial test

--- tests/index.js
+++ tests/index.js
@@ -5,9 +5,13 @@ const mma = require('../index');
 let MMA8541 = new mma.MMA8541(mma.i2cAddress.DEFAULT_ADDRESS);
 
 async function main() {
-  await MMA8541.init();
-  const d = await MMA8541.getAcceleration();
-  console.log(d);
+  try {
+    await MMA8541.init();
+    const d = await MMA8541.getAcceleration();
+    console.log(d);
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 main();
