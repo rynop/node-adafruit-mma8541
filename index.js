a12fe2ab587157ec7fb75b61cbc96c952a2f26c7
rynop <ryan.pendergast@gmail.com>
2017-08-03 22:22:43 -0500

    initial test

--- index.js
+++ index.js
@@ -29,6 +29,15 @@ const ctrlReg2Cmds = {
 const RANGE_4G = 0b01; // +/- 4g.  Must be this if using low noise
 const DEVICE_ID = 0x1A;
 
+uint16(msb, lsb) {
+  return msb << 8 | lsb;
+}
+
+int16(msb, lsb) {
+  const val = uint16(msb, lsb);
+  return val > 32767 ? (val - 65536) : val;
+}
+
 module.exports.registers = registers;
 module.exports.i2cAddress = i2cAddress;
 module.exports.ctrlReg2Cmds = ctrlReg2Cmds;
@@ -42,8 +51,7 @@ module.exports.MMA8541 = class MMA8541 {
   _readRegister(reg) {
     return new Promise((resolve, reject) => {
       this.i2cBus.readByte(this.i2cAddress, reg, (err, byte) => {
-        if (err) return reject(err);
-        return resolve(byte);
+        (err) ? reject(err): resolve(byte);
       })
     });
   }
@@ -51,8 +59,15 @@ module.exports.MMA8541 = class MMA8541 {
   _writeRegister(reg, byte) {
     return new Promise((resolve, reject) => {
       this.i2cBus.writeByte(this.i2cAddress, reg, byte, (err) => {
-        if (err) return reject(err);
-        return true;
+        (err) ? reject(err): resolve(true);
+      })
+    });
+  }
+
+  _readBlock(reg) {
+    return new Promise((resolve, reject) => {
+      this.i2cBus.readI2cBlock(this.i2cAddress, reg, 6, new Buffer(6), (err, bytesRead, buf) => {
+        (err) ? reject(err): resolve(buf);
       })
     });
   }
@@ -79,5 +94,26 @@ module.exports.MMA8541 = class MMA8541 {
     // Activate at max rate, low noise mode. Requires 4G mode
     await this._writeRegister(registers.XYZ_DATA_CFG, RANGE_4G);
     await this._writeRegister(registers.CTRL_REG1, 0x01 | 0x04);
+  }
+
+  async getAcceleration(gForce) { //Default is m/s²
+    await this._writeRegister(registers.OUT_X_MSB, 0);
+    const buffer = this._readBlock(registers.OUT_X_MSB);
+
+    const x = int16(buffer[1], buffer[0]),
+      y = int16(buffer[3], buffer[2]),
+      z = int16(buffer[5], buffer[4]);
+
+    resolve({
+      x: gForce ? x / 2048 : x,
+      y: gForce ? y / 2048 : y,
+      z: gForce ? z / 2048 : z,
+      units: gForce ? 'g' : 'm/s²'
+    });
+  }
+
+  async getOrientation() {
+    const o = await this._readRegister(registers.PL_STATUS) & 0x07;
+    return o;
   }
 };
--- tests/index.js
+++ tests/index.js
@@ -5,3 +5,5 @@ const mma = require('../index');
 let MMA8541 = new mma.MMA8541(mma.i2cAddress.DEFAULT_ADDRESS);
 
 MMA8541.init();
+const d = MMA8541.getAcceleration();
+console.log(d);
