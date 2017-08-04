d5fc8a58f7217bd2f20ff3abc6882fec0214b566
rynop <ryan.pendergast@gmail.com>
2017-08-03 22:40:01 -0500

    initial test

--- tests/index.js
+++ tests/index.js
@@ -7,11 +7,14 @@ let MMA8541 = new mma.MMA8541(mma.i2cAddress.DEFAULT_ADDRESS);
 async function main() {
   try {
     await MMA8541.init();
-    const d = await MMA8541.getAcceleration();
-    console.log(d);
-
     const o = await MMA8541.getOrientation();
-    console.log(o);
+    console.log('orientation', o);
+
+    for (let i in Array(10).fill(0)) {
+      const d = await MMA8541.getAcceleration();
+      console.log(d);
+      await new Promise(resolve => setTimeout(resolve, 500));
+    }
   } catch (e) {
     console.error(e);
   }
